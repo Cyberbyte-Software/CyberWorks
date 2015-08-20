c65dba11952004b97a42f1cf6277a590c6c63250
MightySCollins <theatrepro11@gmail.com>
2015-08-20 22:04:24 +0000

    Login Fix

--- views/core/login.php
+++ views/core/login.php
@@ -216,6 +216,7 @@ if (isset($_GET['token']) && isset($_GET['uID']) && $settings['passreset']) {
     </div>
 </div>
 
+<script type="text/javascript" src="<?php echo $settings['url'] ?>assets/js/main.min.js"></script>
 <script type="text/javascript" src="<?php echo $settings['url'] ?>assets/js/jquery.backstretch.min.js"></script>
 <script>
     $.backstretch([
