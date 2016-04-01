17ec329ea46cfd2c25d9175d8ddbafd5e7a4c38f
cammygames <cammygames@users.noreply.github.com>
2016-04-01 21:07:13 +0100

    YOU CAN HIT ENTER NOW @BoGuu

--- views/templates/search.php
+++ views/templates/search.php
@@ -7,10 +7,10 @@
 </style>
 
 <div class="form-inline pull-right-md">
-    <form name='rows' method="post" action="<?php echo $settings['url'] . $currentPage ?>">
+    <form id="search" name='rows' method="post" action="<?php echo $settings['url'] . $currentPage ?>">
 
             <input id='searchText' class="form-control" type='text' name='searchText' placeholder="<?php if (isset($search)) echo $search ?>">
-            <input style='margin-right: 10px;' class='btn btn-primary' type='button' name='search' onclick='searchpage();' value='<?php echo $lang['search'] ?>'>
+            <input id="searchButton" style='margin-right: 10px;' class='btn btn-primary' type='button' name='search' onclick='searchpage();' value='<?php echo $lang['search'] ?>'>
     </form>
     <select style="margin-right:15px;" onchange="if (this.value) window.location.href='?items='+this.value" id='items' class="form-control" name='items'>
         <?php
@@ -22,3 +22,11 @@
         ?>
     </select>
 </div>
+<script>
+    document.getElementById('searchText').addEventListener('keypress', function(event) {
+        if (event.keyCode == 13) {
+            event.preventDefault();
+            document.getElementById('searchButton').click();
+        }
+    });
+</script>
