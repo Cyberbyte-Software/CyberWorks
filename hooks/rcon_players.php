ba6992e902ec18f49f4143f953b0b0dcf9e21317
PyRowMan <vaillentin@gmail.com>
2017-03-04 13:27:40 +0100

    Remove ancient commented code

--- hooks/rcon_players.php
+++ hooks/rcon_players.php
@@ -13,19 +13,6 @@ if (isset($_GET['sid'])) {
         {
             $rcon = new \Nizarii\ARC(decrypt($server->sq_ip), decrypt($server->rcon_pass), (int)decrypt($server->sq_port));
             $answer = $rcon->getPlayersArray();
-//            var_dump($answer);
-//            $k = strrpos($answer, "---");
-//            $l = strrpos($answer, "(");
-//            $out = substr($answer, $k + 4, $l - $k - 5);
-//            $array = preg_split('/$\R?^/m', $out);
-//            $playersr = array();
-//
-//            if ($array[0] == '(0 players in total')  $array = array();
-//
-//            for ($i = 0; $i < count($array); $i++)
-//            {
-//                $playersr[$i] = array_values(array_diff(explode(' ', $array[$i]), array(null)));
-//            }
 
             echo json_encode($answer);
 
